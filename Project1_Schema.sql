50d6406c138004dd63ba89dd36cd18704b25b32d
andy <ashiraki@csumb.edu>
2022-01-23 21:49:06 -0800

    update constraints

--- Project1_Schema.sql
+++ Project1_Schema.sql
@@ -18,10 +18,10 @@ USE `OtterBot_Pharma` ;
 -- Table `OtterBot_Pharma`.`doctor`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `OtterBot_Pharma`.`doctor` (
-  `doctorssn` VARCHAR(9) NOT NULL,
+  `doctorssn` VARCHAR(11) NOT NULL,
+  `doctorname` VARCHAR(45) NOT NULL,
+  `specialty` VARCHAR(45) NOT NULL,
   `licensedate` DATE NOT NULL,
-  `specialty` VARCHAR(45) NULL,
-  `doctorname` VARCHAR(45) NULL,
   PRIMARY KEY (`doctorssn`))
 ENGINE = InnoDB;
 
@@ -30,17 +30,17 @@ ENGINE = InnoDB;
 -- Table `OtterBot_Pharma`.`patient`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `OtterBot_Pharma`.`patient` (
-  `patientssn` VARCHAR(9) NOT NULL,
-  `doctorssn` VARCHAR(11) NOT NULL,
+  `patientssn` VARCHAR(11) NOT NULL,
   `patientname` VARCHAR(45) NOT NULL,
   `dob` DATE NOT NULL,
   `address` VARCHAR(45) NOT NULL,
+  `doctorssn` VARCHAR(11) NOT NULL,
   PRIMARY KEY (`patientssn`),
-  CONSTRAINT `pimary_care_giver`
+  CONSTRAINT `fk_patient_doctor`
     FOREIGN KEY (`doctorssn`)
     REFERENCES `OtterBot_Pharma`.`doctor` (`doctorssn`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT)
 ENGINE = InnoDB;
 
 
@@ -48,8 +48,8 @@ ENGINE = InnoDB;
 -- Table `OtterBot_Pharma`.`pharmcompany`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `OtterBot_Pharma`.`pharmcompany` (
-  `pharmcompanyname` VARCHAR(50) NOT NULL,
-  `phonenumber` VARCHAR(10) NOT NULL,
+  `pharmcompanyname` VARCHAR(25) NOT NULL,
+  `phone` VARCHAR(10) NOT NULL,
   PRIMARY KEY (`pharmcompanyname`))
 ENGINE = InnoDB;
 
@@ -58,15 +58,15 @@ ENGINE = InnoDB;
 -- Table `OtterBot_Pharma`.`drug`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `OtterBot_Pharma`.`drug` (
-  `name` VARCHAR(50) NOT NULL,
-  `pharmcompanyname` VARCHAR(50) NOT NULL,
-  `genericformula` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`name`),
-  CONSTRAINT `fk_Drug_Supplier1`
+  `drugtradename` VARCHAR(45) NOT NULL,
+  `genericformula` VARCHAR(45) NOT NULL,
+  `pharmcompanyname` VARCHAR(25) NOT NULL,
+  PRIMARY KEY (`drugtradename`),
+  CONSTRAINT `fk_drug_pharmcompany`
     FOREIGN KEY (`pharmcompanyname`)
     REFERENCES `OtterBot_Pharma`.`pharmcompany` (`pharmcompanyname`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
@@ -74,10 +74,10 @@ ENGINE = InnoDB;
 -- Table `OtterBot_Pharma`.`pharmacy`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `OtterBot_Pharma`.`pharmacy` (
-  `phone_number` VARCHAR(10) NOT NULL,
+  `pharmacyphone` VARCHAR(10) NOT NULL,
   `name` VARCHAR(45) NOT NULL,
   `address` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`phone_number`))
+  PRIMARY KEY (`pharmacyphone`))
 ENGINE = InnoDB;
 
 
@@ -85,55 +85,55 @@ ENGINE = InnoDB;
 -- Table `OtterBot_Pharma`.`rxprice`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `OtterBot_Pharma`.`rxprice` (
-  `pharmacyphonenumber` VARCHAR(10) NOT NULL,
-  `drug_name` VARCHAR(50) NOT NULL,
+  `pharmacyphone` VARCHAR(10) NOT NULL,
+  `drugtradename` VARCHAR(45) NOT NULL,
   `price` DECIMAL(7,2) NOT NULL,
-  PRIMARY KEY (`drug_name`, `pharmacyphonenumber`),
-  CONSTRAINT `fk_Pharmacy_has_Drug_Pharmacy1`
-    FOREIGN KEY (`pharmacyphonenumber`)
-    REFERENCES `OtterBot_Pharma`.`pharmacy` (`phone_number`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Pharmacy_has_Drug_Drug1`
-    FOREIGN KEY (`drug_name`)
-    REFERENCES `OtterBot_Pharma`.`drug` (`name`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`drugtradename`, `pharmacyphone`),
+  CONSTRAINT `fk_rxprice_pharmacyphone`
+    FOREIGN KEY (`pharmacyphone`)
+    REFERENCES `OtterBot_Pharma`.`pharmacy` (`pharmacyphone`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_rxprice_drugtradename`
+    FOREIGN KEY (`drugtradename`)
+    REFERENCES `OtterBot_Pharma`.`drug` (`drugtradename`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `OtterBot_Pharma`.`rx`
+-- Table `OtterBot_Pharma`.`prescription`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `OtterBot_Pharma`.`rx` (
+CREATE TABLE IF NOT EXISTS `OtterBot_Pharma`.`prescription` (
   `rxnumber` INT NOT NULL AUTO_INCREMENT,
   `patientssn` VARCHAR(11) NOT NULL,
-  `drugname` VARCHAR(45) NOT NULL,
-  `doctorssn` VARCHAR(9) NOT NULL,
+  `doctorssn` VARCHAR(11) NOT NULL,
+  `drugtradename` VARCHAR(45) NOT NULL,
   `quantity` INT NOT NULL,
-  `pharmacyphonenumber` VARCHAR(10) NOT NULL,
+  `pharmacyphone` VARCHAR(10) NULL,
   `filleddate` DATE NULL,
   PRIMARY KEY (`rxnumber`),
-  CONSTRAINT `fk_Patient_has_Doctor_Patient1`
+  CONSTRAINT `fk_prescription_patientssn`
     FOREIGN KEY (`patientssn`)
     REFERENCES `OtterBot_Pharma`.`patient` (`patientssn`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Patient_has_Doctor_Doctor1`
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
+  CONSTRAINT `fk_prescription_doctorssn`
     FOREIGN KEY (`doctorssn`)
     REFERENCES `OtterBot_Pharma`.`doctor` (`doctorssn`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Prescription_Pharmacy1`
-    FOREIGN KEY (`pharmacyphonenumber`)
-    REFERENCES `OtterBot_Pharma`.`pharmacy` (`phone_number`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Prescription_Drug1`
-    FOREIGN KEY (`drugname`)
-    REFERENCES `OtterBot_Pharma`.`drug` (`name`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
+  CONSTRAINT `fk_prescription_pharmacyphone`
+    FOREIGN KEY (`pharmacyphone`)
+    REFERENCES `OtterBot_Pharma`.`pharmacy` (`pharmacyphone`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_prescription_drugtradename`
+    FOREIGN KEY (`drugtradename`)
+    REFERENCES `OtterBot_Pharma`.`drug` (`drugtradename`)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT)
 ENGINE = InnoDB;
 
 
@@ -151,26 +151,27 @@ ENGINE = InnoDB;
 -- Table `OtterBot_Pharma`.`contract`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `OtterBot_Pharma`.`contract` (
-  `pharmacyphonenumber` VARCHAR(10) NOT NULL,
-  `pharmcompanyname` VARCHAR(50) NOT NULL,
+  `pharmacyphone` VARCHAR(10) NOT NULL,
+  `pharmcompanyname` VARCHAR(25) NOT NULL,
   `supervisorid` INT NOT NULL,
-  `enddate` DATE NOT NULL,
+  `contractualterm` VARCHAR(45) NOT NULL,
   `startdate` DATE NOT NULL,
-  PRIMARY KEY (`pharmacyphonenumber`, `pharmcompanyname`),
-  CONSTRAINT `fk_Pharmacy_has_pharm_company_Pharmacy1`
-    FOREIGN KEY (`pharmacyphonenumber`)
-    REFERENCES `OtterBot_Pharma`.`pharmacy` (`phone_number`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Pharmacy_has_pharm_company_pharm_company1`
+  `enddate` DATE NOT NULL,
+  PRIMARY KEY (`pharmacyphone`, `pharmcompanyname`),
+  CONSTRAINT `fk_contract_pharmacyphone`
+    FOREIGN KEY (`pharmacyphone`)
+    REFERENCES `OtterBot_Pharma`.`pharmacy` (`pharmacyphone`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_contract_pharmcompanyname`
     FOREIGN KEY (`pharmcompanyname`)
     REFERENCES `OtterBot_Pharma`.`pharmcompany` (`pharmcompanyname`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Pharmacy_has_pharm_company_table11`
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_contract_supervisorid`
     FOREIGN KEY (`supervisorid`)
     REFERENCES `OtterBot_Pharma`.`supervisor` (`supervisorid`)
-    ON DELETE CASCADE
+    ON DELETE RESTRICT
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
