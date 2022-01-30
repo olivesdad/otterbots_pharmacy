30c01f85e9e9ce4b3a318b4e132d8ae8034291ad
andy <ashiraki@csumb.edu>
2022-01-30 01:07:07 -0800

    add auto AI IDS

--- Project1_Schema.sql
+++ Project1_Schema.sql
@@ -1,3 +1,4 @@
+
 -- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
@@ -18,11 +19,12 @@ USE `OtterBot_Pharma` ;
 -- Table `OtterBot_Pharma`.`doctor`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `OtterBot_Pharma`.`doctor` (
+  `doctoid` INT NOT NULL AUTO_INCREMENT,
   `doctorssn` CHAR(11) NOT NULL,
   `doctorname` VARCHAR(45) NOT NULL,
   `specialty` VARCHAR(45) NOT NULL,
   `licensedate` DATE NOT NULL,
-  PRIMARY KEY (`doctorssn`))
+  PRIMARY KEY (`doctoid`))
 ENGINE = InnoDB;
 
 
@@ -30,15 +32,16 @@ ENGINE = InnoDB;
 -- Table `OtterBot_Pharma`.`patient`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `OtterBot_Pharma`.`patient` (
+  `patientid` INT NOT NULL AUTO_INCREMENT,
   `patientssn` CHAR(11) NOT NULL,
   `patientname` VARCHAR(45) NOT NULL,
   `dob` DATE NOT NULL,
   `address` VARCHAR(45) NOT NULL,
-  `doctorssn` CHAR(11) NOT NULL,
-  PRIMARY KEY (`patientssn`),
-  CONSTRAINT `fk_patient_doctor`
-    FOREIGN KEY (`doctorssn`)
-    REFERENCES `OtterBot_Pharma`.`doctor` (`doctorssn`)
+  `doctoid` INT NOT NULL,
+  PRIMARY KEY (`patientid`),
+  CONSTRAINT `fk_patient_doctorid`
+    FOREIGN KEY (`doctoid`)
+    REFERENCES `OtterBot_Pharma`.`doctor` (`doctoid`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT)
 ENGINE = InnoDB;
@@ -48,9 +51,10 @@ ENGINE = InnoDB;
 -- Table `OtterBot_Pharma`.`pharmcompany`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `OtterBot_Pharma`.`pharmcompany` (
-  `pharmcompanyname` VARCHAR(25) NOT NULL,
+  `pharmid` INT NOT NULL AUTO_INCREMENT,
   `phone` CHAR(10) NOT NULL,
-  PRIMARY KEY (`pharmcompanyname`))
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`pharmid`))
 ENGINE = InnoDB;
 
 
@@ -60,11 +64,11 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `OtterBot_Pharma`.`drug` (
   `drugtradename` VARCHAR(45) NOT NULL,
   `genericformula` VARCHAR(45) NOT NULL,
-  `pharmcompanyname` VARCHAR(25) NOT NULL,
+  `pharmid` INT NOT NULL,
   PRIMARY KEY (`drugtradename`),
-  CONSTRAINT `fk_drug_pharmcompany`
-    FOREIGN KEY (`pharmcompanyname`)
-    REFERENCES `OtterBot_Pharma`.`pharmcompany` (`pharmcompanyname`)
+  CONSTRAINT `fk_drug_pharmid`
+    FOREIGN KEY (`pharmid`)
+    REFERENCES `OtterBot_Pharma`.`pharmcompany` (`pharmid`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
@@ -108,23 +112,13 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `OtterBot_Pharma`.`prescription` (
   `rxnumber` INT NOT NULL AUTO_INCREMENT,
-  `patientssn` CHAR(11) NOT NULL,
-  `doctorssn`  CHAR(11) NOT NULL,
+  `doctor_doctoid` INT NOT NULL,
   `drugtradename` VARCHAR(45) NOT NULL,
   `quantity` INT NOT NULL,
   `pharmacyphone` VARCHAR(10) NULL,
   `filleddate` DATE NULL,
-  PRIMARY KEY (`rxnumber`),
-  CONSTRAINT `fk_prescription_patientssn`
-    FOREIGN KEY (`patientssn`)
-    REFERENCES `OtterBot_Pharma`.`patient` (`patientssn`)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT,
-  CONSTRAINT `fk_prescription_doctorssn`
-    FOREIGN KEY (`doctorssn`)
-    REFERENCES `OtterBot_Pharma`.`doctor` (`doctorssn`)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT,
+  `patient_patientid` INT NOT NULL,
+  PRIMARY KEY (`rxnumber`, `doctor_doctoid`, `patient_patientid`),
   CONSTRAINT `fk_prescription_pharmacyphone`
     FOREIGN KEY (`pharmacyphone`)
     REFERENCES `OtterBot_Pharma`.`pharmacy` (`pharmacyphone`)
@@ -134,7 +128,17 @@ CREATE TABLE IF NOT EXISTS `OtterBot_Pharma`.`prescription` (
     FOREIGN KEY (`drugtradename`)
     REFERENCES `OtterBot_Pharma`.`drug` (`drugtradename`)
     ON DELETE RESTRICT
-    ON UPDATE RESTRICT)
+    ON UPDATE RESTRICT,
+  CONSTRAINT `fk_prescription_doctorid`
+    FOREIGN KEY (`doctor_doctoid`)
+    REFERENCES `OtterBot_Pharma`.`doctor` (`doctoid`)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
+  CONSTRAINT `fk_prescription_patientid`
+    FOREIGN KEY (`patient_patientid`)
+    REFERENCES `OtterBot_Pharma`.`patient` (`patientid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -153,20 +157,20 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `OtterBot_Pharma`.`contract` (
   `pharmacyphone` CHAR(10) NOT NULL,
-  `pharmcompanyname` VARCHAR(25) NOT NULL,
+  `pharmid` INT NOT NULL,
   `supervisorid` INT NOT NULL,
   `contractualterm` VARCHAR(45) NOT NULL,
   `startdate` DATE NOT NULL,
   `enddate` DATE NOT NULL,
-  PRIMARY KEY (`pharmacyphone`, `pharmcompanyname`),
+  PRIMARY KEY (`pharmacyphone`, `pharmid`),
   CONSTRAINT `fk_contract_pharmacyphone`
     FOREIGN KEY (`pharmacyphone`)
     REFERENCES `OtterBot_Pharma`.`pharmacy` (`pharmacyphone`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
-  CONSTRAINT `fk_contract_pharmcompanyname`
-    FOREIGN KEY (`pharmcompanyname`)
-    REFERENCES `OtterBot_Pharma`.`pharmcompany` (`pharmcompanyname`)
+  CONSTRAINT `fk_contract_pharmid`
+    FOREIGN KEY (`pharmid`)
+    REFERENCES `OtterBot_Pharma`.`pharmcompany` (`pharmid`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
   CONSTRAINT `fk_contract_supervisorid`
@@ -181,36 +185,3 @@ SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
--- SQL statement 1 --
-SELECT p.patientname, p.doctorssn, d.doctorname, rx.price
-FROM patient p,doctor d,rxprice rx
-ORDER BY patientname ;
-
--- SQL statement 2 --
-SELECT drugtradename,price
-FROM rxprice
-WHERE price= (SELECT MIN(price) FROM rxprice)
-ORDER BY price;
-
--- SQL statement 3 ---
-SELECT patientname,patientssn, d.doctorssn, d.doctorname, d.specialty
-FROM patient, doctor d
-WHERE d.doctorssn IN (SELECT d.doctorssn
-FROM doctor d
-WHERE specialty = "Pediatrics");
-
--- SQL statement 4 --
-SELECT r.drugtradename, r.price, p.pharmacyphone
-FROM prescription p
-JOIN drug d on p.drugtradename = d.drugtradename
-JOIN rxprice r on d.drugtradename = r.drugtradename
-WHERE p.filleddate IS NOT NULL
-ORDER BY r.drugtradename;
-
--- SQL statement 5 --
-SELECT patientname, doctorname, drug.drugtradename
-FROM prescription rx
-JOIN patient p ON rx.patientssn = p.patientssn
-JOIN doctor d ON rx.doctorssn = d.doctorssn
-JOIN drug ON rx.drugtradename = drug.drugtradename
-WHERE filleddate IS NULL;
