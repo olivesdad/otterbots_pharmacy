48d8fc1877a3915dfc51307ceaa4e00b4ab5ed63
andy <ashiraki@csumb.edu>
2022-02-05 18:51:57 -0800

    notes to the sanitizer

--- Project2/src/main/java/com/csumb/cst363/TheSanitizer.java
+++ Project2/src/main/java/com/csumb/cst363/TheSanitizer.java
@@ -6,40 +6,43 @@ import java.util.regex.Pattern;
 
 public class TheSanitizer {
 
+    /**
+     * This accepts a string and returns true if it's loosely and address.
+     * we allow any combinations of alphas and numerics with the addition of dashes (-) periods (.) and commas (,)
+     */
     public static boolean isAddress(String s) {
         String regex = "[0-9a-zA-Z ,\\-\\.]+";
         if (Pattern.matches(regex, s)) return true;
         else return false;
     }
 
+    /**
+     * This accepts a string and returns true if its loosely a name
+     * we accept any combination of alphas with the addition of dashes (-) and appostrophies (')
+     * the appostrophies are questionable for sql statements
+     */
     public static boolean isName(String s) {
         String regex = "[a-z A-Z\\-']+";
        if (Pattern.matches(regex,s))return true;
        else return false;
     }
 
+    /**
+     * This accepts a string and returns true if its a valid ssn in the form ###-##-####
+     */
     public static boolean isSSN(String s){
         String regex = "^(?!000|666)[0-8][0-9]{2}-(?!00)[0-9]{2}-(?!0000)[0-9]{4}$";
         if(Pattern.matches(regex,s )) return true;
         else return false;
     }
 
+    /**
+     * Accepts a string and returns true if it is a valid zipcode which consists of 5 numerics excluding 00000
+     */
     public static boolean isZip(String s){
         String regex ="(?!00000)[0-9]{5}";
         if (Pattern.matches(regex, s)) return true;
         else return false;
     }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("enter: ");
-        String s = scanner.nextLine();
-        System.out.println(isAddress(s));
-        System.out.println(isSSN(s));
-        System.out.println(isName(s));
-        System.out.println(isZip(s));
-
-    }
-
-
 
 }
