4813cca8c3d32803d1be81cc5f57aa21d6545c10
andy <ashiraki@csumb.edu>
2022-02-06 17:33:15 -0800

    add the sanitizer to datagenerate

--- testing.sql
+++ testing.sql
@@ -1,8 +1,8 @@
 insert into patient values (1, '222-22-2222', 'name', '1995-12-12', '123 street', 1);
 
+delete from prescription;
 delete from patient;
 delete from doctor;
-delete from prescription;
 
 insert into patient (ssn, name, dob, address, doctorid) values ('123-22-2222' , 'bobby benson', '1999-04-04', '12 boo street', 5);
 
